7c56754c50b5f65479b69740f7575f4f289fc678
Daniel.Laga <daniellaga84@gmail.com>
2021-06-01 19:11:29 +0200

    Initial commit.

---
+++ src/main/java/com/example/demo/Springo2AuthwithFacebookApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Springo2AuthwithFacebookApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Springo2AuthwithFacebookApplication.class, args);
+	}
+
+}
